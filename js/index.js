eb732b1a39cb7ce33b75484614162b88970118e3
seifmuhammedd <seifamuhammed2@gmail.com>
2024-10-28 20:54:01 +0300

    added contact section

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
 /// <reference types="../@types/jquery" />
 
-$(".open span").on("click",function(){
-    $("aside").css("left" , "0")
-})
+$(".open span").on("click", function () {
+  $("aside").css("left", "0");
+});
 
-$(".close").on("click",function(){
-    let sideBarWidth = $("aside").innerWidth()
-    $("aside").css("left" , -sideBarWidth)
-})
+$(".close").on("click", function () {
+  let sideBarWidth = $("aside").innerWidth();
+  $("aside").css("left", -sideBarWidth);
+});
 
-$(".singers h3").on("click",function(){
-    $(this).next().slideToggle(1000)
-    $(".singers .slider").not($(this).next()).slideUp(1000)
-})
+// Scroll
+// $(".sidebar .links a").on("click",function(){
+//     let sectionID = $(this).attr("href")
+//     let sectionOffset = $(sectionID).offset().top
+//     $("body,html").animate({scrollTop:sectionOffset},1000)
+// })
 
-$('#getting-started').countdown('2024/11/09', function(event) {
-    $("#day").html(event.strftime(' %D D'));
-    $("#hour").html(event.strftime('%H H'));
-    $("#minute").html(event.strftime('%M M'));
-    $("#seconed").html(event.strftime('%S S'));
-  });
+$(".singers h3").on("click", function () {
+  $(this).next().slideToggle(1000);
+  $(".singers .slider").not($(this).next()).slideUp(1000);
+});
+
+$("#getting-started").countdown("2024/11/09", function (event) {
+  $("#day").html(event.strftime(" %D D"));
+  $("#hour").html(event.strftime("%H H"));
+  $("#minute").html(event.strftime("%M M"));
+  $("#seconed").html(event.strftime("%S S"));
+});
+
+$("textarea").on("keyup" , function(){
+    let textLength = $(this).val().length;
+    if (textLength >= 100){
+        $(".counter").html("No characters remaining").css("color" , "red")
+    }else{
+        $(".counter").html(100-textLength)
+    }
+})
